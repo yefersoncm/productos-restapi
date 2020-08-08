0a40b1ccdd3c8b165140a2f679a34f9efab2f898
yefersoncm <yefersoncm@users.noreply.github.com>
2020-08-07 20:42:50 -0500

    Create productos.py
    Practice, python API updated

---
+++ productos.py
@@ -0,0 +1,8 @@
+productos = [
+	{"nombre":"laptop", "precio": 800, "cantidad": 40},
+	{"nombre":"mouse", "precio": 40, "cantidad": 40},
+	{"nombre":"monitor", "precio": 200, "cantidad": 40},
+	{"nombre":"ram", "precio": 50, "cantidad": 40},
+	{"nombre":"teclado", "precio": 35, "cantidad": 8}
+
+]
